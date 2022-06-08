80e6de4b4ae9668e2464e8a5d22bd44d21bff08c
Fausto Alejandro Guano Rojas <fguano@dmq.quitodm.inst>
2022-06-08 09:58:01 -0500

    Carga de página

---
+++ pointclouds/indexl/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 88237413,
+    "boundingBox": {
+        "lx": 503007.953,
+        "ly": 9970362.336,
+        "lz": 2369.369,
+        "ux": 505308.173,
+        "uy": 9972662.556,
+        "uz": 4669.589000000031
+    },
+    "tightBoundingBox": {
+        "lx": 503007.953,
+        "ly": 9970362.336,
+        "lz": 2369.369,
+        "ux": 505308.173,
+        "uy": 9971943.276,
+        "uz": 2650.808
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 19.920490264892579,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
